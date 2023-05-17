5db1322a7eb0a9c488ef10e5ea3f8b1b46138b88
Andres Armendariz <andres.armendariz@ioet.com>
2023-05-17 12:46:40 -0500

    test: Write a failling test

---
+++ 1. Developer basics/Data types and Data Structures/Dictionary/exercise_1/exercise_1.py
@@ -0,0 +1,36 @@
+
+#Define the Dictionary
+from collections import defaultdict
+frecuency:dict = defaultdict(int)
+path:str =  "/Users/ioet/Documents/ioet-Andres Armendariz/1. Developer basics/Data types and Data Structures/Dictionary/exercise_1/file.txt"
+
+#Open and read the file 
+def read_file(path:str)->str:
+    with open(path) as file:
+        text = file.read()
+    return text
+
+#Transform all the string in words
+def split_in_words(text: str):
+    words:str = text.split()
+    return words
+
+#iterate and count frecuency words
+def count_words(words: str):
+    for word in words:
+        if word not in frecuency:
+            frecuency[word] = 1
+        else:
+            frecuency[word] += 1
+
+#Show the results
+def show_results():
+    for word in frecuency:
+        print(word,frecuency[word])
+
+print("Las palabras y sus frecuencias son: ")
+text:str = read_file(path)
+words:str = split_in_words(text)
+count_words(words)
+show_results()
+
---
+++ 1. Developer basics/Data types and Data Structures/Dictionary/exercise_1/tdd_1.py
@@ -0,0 +1,10 @@
+import pytest
+
+import exercise_1 
+
+
+def test__read_text_file():
+    file = "/Users/ioet/Documents/ioet-Andres Armendariz/1. Developer basics/Data types and Data Structures/Dictionary/exercise_1/test_file.txt"
+    text_expected = ""
+    text = exercise_1.read_file(file)
+    assert text_expected == text
