a6ea07e7645a17fd5b8b4c92f4c937352e954ae9
Andres Armendariz <andres.armendariz@ioet.com>
2023-05-18 14:34:36 -0500

    test: write a passing test for value_conversion()

--- 1. Developer basics/Data types and Data Structures/Dictionary/exercise_2/tdd_2.py
+++ 1. Developer basics/Data types and Data Structures/Dictionary/exercise_2/tdd_2.py
@@ -7,7 +7,8 @@ import exercise_2
 def test__value_conversion__return_a_string__when_inputs_are_dictionary_value_and_measure():
     input_value = float(25)
     measure = "km"
-    value_expected = [25000, 250000, 2500000]
+    value_expected = [25000.0,250000.0,25000000.0]
     dictionary = {"km": {"m": 1000, "cm": 10000, "mm": 1000000}}
     value = exercise_2.value_conversion(dictionary, input_value, measure)
     assert value_expected == value
+
