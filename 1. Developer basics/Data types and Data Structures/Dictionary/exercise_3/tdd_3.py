d209cc7071ddef8aeaa0e78f6e3af0065df64d70
Andres Armendariz <andres.armendariz@ioet.com>
2023-05-19 12:30:20 -0500

    refactor: refact test for funtion students_average()

--- 1. Developer basics/Data types and Data Structures/Dictionary/exercise_3/tdd_3.py
+++ 1. Developer basics/Data types and Data Structures/Dictionary/exercise_3/tdd_3.py
@@ -3,23 +3,25 @@ import pytest
 import exercise_3
 
 def test__save_grades__return_dictionay__when_inputs_are_dictionary_of_students_and_student():
-    students = {"student_1":{"grade_1":0,"grade_2":0,"grade_3":0,"grade_4":0,"grade_5":0},
-                "student_2":{"grade_1":0,"grade_2":0,"grade_3":0,"grade_4":0,"grade_5":0}}
-    student = "student_1"
-    dictionary_expected = {'student_1':{'grade_1':'10','grade_2':'10','grade_3':'10','grade_4':'10','grade_5':'10'},"student_2":{"grade_1":0,"grade_2":0,"grade_3":0,"grade_4":0,"grade_5":0}}
-    dictionary = exercise_3.save_grades(students, student)
-    assert dictionary_expected == dictionary
+     students = {"student_1":{"grade_1":0,"grade_2":0,"grade_3":0,"grade_4":0,"grade_5":0},
+                 "student_2":{"grade_1":0,"grade_2":0,"grade_3":0,"grade_4":0,"grade_5":0}}
+     student = "student_1"
+     dictionary_expected = {'student_1':{'grade_1':'10','grade_2':'10','grade_3':'10','grade_4':'10','grade_5':'10'},"student_2":{"grade_1":0,"grade_2":0,"grade_3":0,"grade_4":0,"grade_5":0}}
+     dictionary = exercise_3.save_grades(students, student)
+     assert dictionary_expected == dictionary
 
 def test__save_grades_all_students__return_dictionary__when_input_is_dictionary():
+     students = {"student_1":{"grade_1":0,"grade_2":0,"grade_3":0,"grade_4":0,"grade_5":0},
+                 "student_2":{"grade_1":0,"grade_2":0,"grade_3":0,"grade_4":0,"grade_5":0}}
+     dictionary_expected = {"student_1":{"grade_1":'10',"grade_2":'10',"grade_3":'10',"grade_4":'10',"grade_5":'10'},
+                                 "student_2":{"grade_1":'10',"grade_2":'10',"grade_3":'10',"grade_4":'10',"grade_5":'10'}}
+     dictionary = exercise_3.save_grades_all_students(students)
+     assert dictionary_expected == dictionary
+
+def test__students_average__return_tuple__when_input_is_dictionary_of_students():
     students = {"student_1":{"grade_1":0,"grade_2":0,"grade_3":0,"grade_4":0,"grade_5":0},
                 "student_2":{"grade_1":0,"grade_2":0,"grade_3":0,"grade_4":0,"grade_5":0}}
-    dictionary_expected = {"student_1":{"grade_1":'10',"grade_2":'10',"grade_3":'10',"grade_4":'10',"grade_5":'10'},
-                                "student_2":{"grade_1":'10',"grade_2":'10',"grade_3":'10',"grade_4":'10',"grade_5":'10'}}
-    dictionary = exercise_3.save_grades_all_students(students)
-    assert dictionary_expected == dictionary
-
-def test__students_average__return_dictionary__when_input_is_dictionary_of_students():
     students_average = {"student_1":0, "student_2":0}
     dictionary_exepcted = ""
-    dictionary = exercise_3.studens_average(students_average)
+    dictionary = exercise_3.students_average(students_average, students)
     assert dictionary_exepcted == dictionary
