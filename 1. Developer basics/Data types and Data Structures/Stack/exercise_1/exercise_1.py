2b6f40b2564ad3d2354a2cc7a55c38e8717a031c
Andres Armendariz <andres.armendariz@ioet.com>
2023-05-23 16:55:56 -0500

    fix: delete unnecessary commets

--- 1. Developer basics/Data types and Data Structures/Stack/exercise_1/exercise_1.py
+++ 1. Developer basics/Data types and Data Structures/Stack/exercise_1/exercise_1.py
@@ -1,8 +1,3 @@
-# #EJERCICIO DE VERIFICACION DE PARENTESIS:
-# #Escribe una función que tome una cadena que contiene paréntesis, corchetes y llaves, y verifique si los paréntesis están balanceados. 
-# #Es decir, que cada paréntesis abierto tenga su correspondiente paréntesis cerrado en el orden correcto. Utiliza una pila para llevar un 
-# #seguimiento de los paréntesis abiertos y cierra el último paréntesis abierto si se encuentra un paréntesis cerrado.
-
 
 def add_character(character: str, grouping_signs:str)->str:
     if character in ['(','[','{']:
@@ -21,35 +16,6 @@ def delete_character(character: str, grouping_signs: str)->str:
         if character == '}' and sign != '{':
             print( "La expresion no esta balanceada")
     return grouping_signs      
-            
-# #Function por balanced parentheses
-# def balanced_grouping_signs(string:str):
-   
-#     #Define the stack
-#     grouping_signs:str = []
-
-#     for character in string:
-#         #If the character is a open parentheses add the element to stack
-#         if character in ['(','[','{']:
-#             grouping_signs.append(character)
-
-#             #If the character is a close parentheses and the stack es empy, the string isn't balanced.
-#         elif character in [')',']','}']:
-#             if not grouping_signs:
-#                 print("La expresion no esta balanceada")
-
-#             #If the stack isn't empy, pop the last element from stack until find the corresponding closing sign 
-#             sign = grouping_signs.pop()
-#             if character == ')' and sign != '(':
-#                 return print("La expresion no esta balanceada")
-#             if character == ']' and sign != '[':
-#                 return print("La expresion no esta balanceada")
-#             if character == '}' and sign != '{':
-#                 return print("La expresion no esta balanceada")
-#     if len(grouping_signs) == 0:
-#         return print("La expresion esta balanceada")
-#     else: 
-#         return print("La expresion no esta balanceada")
             
 if __name__ == "__main__":
     #Request a string
