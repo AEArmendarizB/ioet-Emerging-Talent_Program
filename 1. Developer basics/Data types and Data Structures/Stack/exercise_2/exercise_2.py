aae5fbdbcfcb0a14cabfe0f8f8726158efc4ec9c
Andres Armendariz <andres.armendariz@ioet.com>
2023-05-23 18:16:32 -0500

    feat: add the function evaluate_postfix()

--- 1. Developer basics/Data types and Data Structures/Stack/exercise_2/exercise_2.py
+++ 1. Developer basics/Data types and Data Structures/Stack/exercise_2/exercise_2.py
@@ -5,6 +5,21 @@ def is_a_digit(operands:str, character:str)->str:
         number = int(character)
         operands.append(number)
     return operands
+
+def evaluate_postfix(operands:str,character:str)->str:
+    if not character.isdigit():
+        operand_2 = operands.pop()
+        operand_1 = operands.pop()
+        if character == '+':
+            operands.append(operand_1+operand_2)
+        elif character == '-':
+            operands.append(operand_1-operand_2)
+        elif character == '*':
+            operands.append(operand_1*operand_2)
+        elif character == '/':
+            operands.append(operand_1/operand_2)
+    return operands
+
 # #Define a function
 # def evaluate_postfix_expresion(expression: str) ->str:
 #     operands = []
