3b7fe359aaf5ff457a113b3bdb3fba729748e888
Andres Armendariz <andres.armendariz@ioet.com>
2023-05-23 15:34:26 -0500

    test: write a failing test for delete_character()

--- 1. Developer basics/Data types and Data Structures/Stack/exercise_1/tdd_1.py
+++ 1. Developer basics/Data types and Data Structures/Stack/exercise_1/tdd_1.py
@@ -7,3 +7,10 @@ def test__add_character__return_stack__when__input_is_a_string():
     stack_expected = ['(','(','(','[','(','{','{','(']
     stack = exercise_1.add_character(string)
     assert stack_expected == stack
+
+def test__delete_character__return_stack__when_input_is_a_stack_and_string():
+    string = "((([({{()}})])))"
+    stack = ['(','(','(','[','(','{','{','(']
+    stack_deleted_expected = []
+    stack_deleted = exercise_1.delete_character(stack)
+    assert stack_deleted_expected == stack_deleted
