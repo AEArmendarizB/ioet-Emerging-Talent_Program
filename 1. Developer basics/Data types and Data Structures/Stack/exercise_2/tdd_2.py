264fa1961c4ff90dd2353aacbf2a71b6efeaaccf
Andres Armendariz <andres.armendariz@ioet.com>
2023-05-23 18:17:04 -0500

    test: write a failing test for evlaute_postfix()

--- 1. Developer basics/Data types and Data Structures/Stack/exercise_2/tdd_2.py
+++ 1. Developer basics/Data types and Data Structures/Stack/exercise_2/tdd_2.py
@@ -9,7 +9,8 @@ def test__is_a_digit__return_stack__when_inputs_are_stack_and_character():
     operands = exercise_2.is_a_digit(stack,character)
     assert operands_expected == operands
 
-def test__evaluate_postfix__return_stack__when_inputs_is_stack():
+def test__evaluate_postfix__return_stack__when_inputs_are_stack_and_character():
+    character = '2'
     stack = [2,3]
     operands_expected = ""
     operands = exercise_2.evaluate_postfix(stack)
