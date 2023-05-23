1c1bdef2e5c1fb6245c7cc330fb1281178b8e589
Andres Armendariz <andres.armendariz@ioet.com>
2023-05-23 16:48:13 -0500

    test: write a passing test for add_character() and delete_character()

--- 1. Developer basics/Data types and Data Structures/Stack/exercise_1/tdd_1.py
+++ 1. Developer basics/Data types and Data Structures/Stack/exercise_1/tdd_1.py
@@ -3,14 +3,15 @@ import pytest
 import exercise_1
 
 def test__add_character__return_stack__when__input_is_a_string():
-    string = "((([({{()}})])))"
-    stack_expected = ['(','(','(','[','(','{','{','(']
-    stack = exercise_1.add_character(string, string)
+    character = "("
+    grouping_sing = ['(','(']
+    stack_expected = ['(','(','(']
+    stack = exercise_1.add_character(character, grouping_sing)
     assert stack_expected == stack
 
 def test__delete_character__return_stack__when_input_is_a_stack_and_string():
-    string = "((([({{()}})])))"
-    stack = ['(','(','(','[','(','{','{','(']
-    stack_deleted_expected = []
-    stack_deleted = exercise_1.delete_character(string, stack)
+    character = "}"
+    grouping_sing = ['(','(','(']
+    stack_deleted_expected = ['(','(']
+    stack_deleted = exercise_1.delete_character(character, grouping_sing)
     assert stack_deleted_expected == stack_deleted
