8fe9bf1daf26b15b073f3a9021d6c9e399189f15
Andres Armendariz <andres.armendariz@ioet.com>
2023-05-23 17:57:55 -0500

    test: write a passing test for is_a_digit()

--- 1. Developer basics/Data types and Data Structures/Stack/exercise_2/tdd_2.py
+++ 1. Developer basics/Data types and Data Structures/Stack/exercise_2/tdd_2.py
@@ -5,6 +5,7 @@ import exercise_2
 def test__is_a_digit__return_stack__when_inputs_is_stack():
     character = '2'
     stack = []
-    operands_expected=""
-    operands = exercise_2.is_a_digit(character,stack)
+    operands_expected = [2]
+    operands = exercise_2.is_a_digit(stack,character)
     assert operands_expected == operands
+
