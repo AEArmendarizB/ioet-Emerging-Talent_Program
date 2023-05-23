125936a765fd1cbc41bb76ed090874cfe3ce421b
Andres Armendariz <andres.armendariz@ioet.com>
2023-05-23 15:24:00 -0500

    test: write a passing test for add_character()

--- 1. Developer basics/Data types and Data Structures/Stack/exercise_1/tdd_1.py
+++ 1. Developer basics/Data types and Data Structures/Stack/exercise_1/tdd_1.py
@@ -3,7 +3,7 @@ import pytest
 import exercise_1
 
 def test__add_character__return_stack__when__input_is_a_string():
-    string: "((()))"
-    stack_expected = ""
+    string = "((()))"
+    stack_expected = ['(','(','(']
     stack = exercise_1.add_character(string)
     assert stack_expected == stack
