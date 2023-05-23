17e49faee9868bb110f012bb1a1c2659bf849560
Andres Armendariz <andres.armendariz@ioet.com>
2023-05-23 17:02:25 -0500

    test: write a passing test for delete_character()

--- 1. Developer basics/Data types and Data Structures/Stack/exercise_1/tdd_1.py
+++ 1. Developer basics/Data types and Data Structures/Stack/exercise_1/tdd_1.py
@@ -12,6 +12,6 @@ def test__add_character__return_stack__when__input_is_a_string():
 def test__delete_character__return_stack_and_boolean__when_input_is_a_stack_and_string():
     character = "}"
     grouping_sing = ['(','(','(']
-    stack_deleted_expected = ['(','(']
+    stack_deleted_expected = (['(','('], True)
     stack_deleted = exercise_1.delete_character(character, grouping_sing)
     assert stack_deleted_expected == stack_deleted
