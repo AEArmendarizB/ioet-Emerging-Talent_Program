0c3591450e7cd7db4d88ed6570cf54f329a81288
Andres Armendariz <andres.armendariz@ioet.com>
2023-05-23 18:28:15 -0500

    feat: add the result

--- 1. Developer basics/Data types and Data Structures/Stack/exercise_2/exercise_2.py
+++ 1. Developer basics/Data types and Data Structures/Stack/exercise_2/exercise_2.py
@@ -27,30 +27,6 @@ if __name__ == "__main__":
         operands = is_a_digit(operands,character)
         operands = evaluate_postfix (operands,character)
     print ("El valor es: "f"{operands.pop()}")
-# #Define a function
-# def evaluate_postfix_expresion(expression: str) ->str:
-#     operands = []
-#     number = ""
-#     for character in expression:
-#         if character.isdigit():
-#             number = int(character)
-#             operands.append(number)
-#         else:
-#             operand_2 = operands.pop()
-#             operand_1 = operands.pop()
-#             if character == '+':
-#                 operands.append(operand_1+operand_2)
-#             elif character == '-':
-#                 operands.append(operand_1-operand_2)
-#             elif character == '*':
-#                 operands.append(operand_1*operand_2)
-#             elif character == '/':
-#                 operands.append(operand_1/operand_2)    
-#             number=""
-#     return operands.pop()
 
-#     #Request postfix expression
-# expression = input("Ingrese la expresion posfija a ser evaluada: ")
-# print("EL valor es: " f"{evaluate_postfix_expresion(expression)}")
 
 
