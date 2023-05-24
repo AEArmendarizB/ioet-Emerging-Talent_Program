f0a1e0b61a299ae6e5b69faa48df58aed980ea36
Andres Armendariz <andres.armendariz@ioet.com>
2023-05-24 10:47:47 -0500

    test: write a failing test for iterate_stack()

--- 1. Developer basics/Data types and Data Structures/Stack/exercise_3/tdd_3.py
+++ 1. Developer basics/Data types and Data Structures/Stack/exercise_3/tdd_3.py
@@ -2,8 +2,15 @@ import pytest
 
 import exercise_3
 
-def test__reverted_expression__return_string__when_inputs_is_stack():
+def test__reverted_expression__return_stack__when_inputs_is_string():
     string = "Hola"
     expression_expected = ['H','o','l','a']
     expression = exercise_3.reverted_expression(string)
     assert expression_expected == expression
+
+def test__iterate_stack__return_string__when_inputs_is_stack():
+    stack = ['H','o','l','a']
+    expression_inverted_expected = ""
+    expression_inverted = exercise_3.iterate_stack(stack)
+    assert expression_inverted_expected == expression_inverted
+    
