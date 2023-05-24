75e843d85015d9592664fb508585819b60698da8
Andres Armendariz <andres.armendariz@ioet.com>
2023-05-24 12:24:58 -0500

    test: write a passing test for add_new_client()

--- 1. Developer basics/Data types and Data Structures/Queue/exercise_1/tdd_1.py
+++ 1. Developer basics/Data types and Data Structures/Queue/exercise_1/tdd_1.py
@@ -2,9 +2,25 @@ import pytest
 from queue import Queue
 import exercise_1
 
-def test__add_new_client__return_queue__when_input_is_a_int():
+#It help for comparate the content of the queue and not the references
+class QueueWithContentComparison(Queue):
+    def __eq__(self, other):
+        if isinstance(other, QueueWithContentComparison):
+            return list(self.queue) == list(other.queue)
+        return False
+
+def test__add_new_client__return_queue__when_input_is_are_queue_and_int():
     client = 1
-    queue_client = Queue()
-    queue_client.put(client)
-    queue_client_expected = exercise_1.add_new_client(client)
-    assert queue_client_expected == queue_client
+    supermarket = QueueWithContentComparison()
+    supermarket_client_expected = QueueWithContentComparison()
+    supermarket_client_expected.put(client)
+    supermarket_client = exercise_1.add_new_client(supermarket, client)
+    assert supermarket_client_expected == supermarket_client
+
+
+
+
+
+
+
+
