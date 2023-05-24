807b19b58dfe37e336398d3edf03b3c4ec146625
Andres Armendariz <andres.armendariz@ioet.com>
2023-05-24 10:42:36 -0500

    test: write a passing test for rverted_expression()

--- 1. Developer basics/Data types and Data Structures/Stack/exercise_3/tdd_3.py
+++ 1. Developer basics/Data types and Data Structures/Stack/exercise_3/tdd_3.py
@@ -2,8 +2,8 @@ import pytest
 
 import exercise_3
 
-def test__reverted_expression__return_string__when_inputs_is_string():
+def test__reverted_expression__return_string__when_inputs_is_stack():
     string = "Hola"
-    expression_expected = ""
-    expression = reverted_expression(string)
+    expression_expected = ['H','o','l','a']
+    expression = exercise_3.reverted_expression(string)
     assert expression_expected == expression
