307bfeffba3e92f70650931fd438360256f24a87
Andres Armendariz <andres.armendariz@ioet.com>
2023-05-24 12:31:39 -0500

    test: write a failling test for attend_client()

--- 1. Developer basics/Data types and Data Structures/Queue/exercise_1/tdd_1.py
+++ 1. Developer basics/Data types and Data Structures/Queue/exercise_1/tdd_1.py
@@ -9,7 +9,7 @@ class QueueWithContentComparison(Queue):
             return list(self.queue) == list(other.queue)
         return False
 
-def test__add_new_client__return_queue__when_input_is_are_queue_and_int():
+def test__add_new_client__return_queue__when_input_are_queue_and_int():
     client = 1
     supermarket = QueueWithContentComparison()
     supermarket_client_expected = QueueWithContentComparison()
@@ -17,7 +17,12 @@ def test__add_new_client__return_queue__when_input_is_are_queue_and_int():
     supermarket_client = exercise_1.add_new_client(supermarket, client)
     assert supermarket_client_expected == supermarket_client
 
-
+def test__attend_client__return_queue__when_input_is_queue():
+    supermarket = QueueWithContentComparison()
+    supermarket.put(2)
+    supermarket_client_expected = QueueWithContentComparison()
+    supermarket_client = exercise_1.attend_client(supermarket)
+    assert supermarket_client_expected == supermarket_client
 
 
 
