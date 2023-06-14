57327072d2efcc81062dcdf357cfd028abfab5ca
Andres Armendariz <andy_armendariz@hotmail.es>
2023-06-14 13:49:58 -0500

    ad

--- 1. Developer basics/Data types and Data Structures/Queue/exercise_2/exercise_2.py
+++ 1. Developer basics/Data types and Data Structures/Queue/exercise_2/exercise_2.py
@@ -1,35 +1,36 @@
-#EJERCICIO DE ORDENAMIENTO POR MEZCLA UTILIZANDO COLAS: 
-#Escribe Escribe una función que implemente el algoritmo de ordenamiento por mezcla utilizando dos colas. La función debe tomar una 
-#lista de números desordenados, y dividirla en dos sub-listas de tamaño aproximadamente igual. Luego, debe ordenar cada sub-lista utilizando 
-#la misma función recursivamente, y finalmente mezclar las dos colas resultantes en una sola cola ordenada.
-
-
 from queue import Queue
 
-#Funtion to separate a string into 2 new strings
 def strings(input_string):
-    if len(input_string)<=1: 
+    if len(input_string) <= 1: 
         return Queue(input_string)
 
-    middle: int = len(input_string)//2
-    left_string:int = input_string[:middle]
-    right_string: int = input_string[middle:]
+    middle = len(input_string) // 2
+    left_string = input_string[:middle]
+    right_string = input_string[middle:]
 
-    #call to recursive funtion
-    left_sorted_string: int = strings(left_string)
-    right_sorted_stirng: int = strings(right_string)
+    left_sorted_string = strings(left_string)
+    right_sorted_stirng = strings(right_string)
 
-    #Join and sort the strings
-    outpu_string = Queue()
-    while left_sorted_string and right_sorted_stirng:
-        if left_sorted_string[0] < right_sorted_stirng[0]:
-            outpu_string.put(left_sorted_string.popleft())
+    output_string = Queue()
+    while not left_sorted_string.empty() and not right_sorted_stirng.empty():
+        if left_sorted_string.queue[0] < right_sorted_stirng.queue[0]:
+            output_string.put(left_sorted_string.get())
         else:
-            outpu_string.put(right_sorted_stirng.popleft())
-    outpu_string.extend(left_sorted_string)
-    outpu_string.extend(right_sorted_stirng)
+            output_string.put(right_sorted_stirng.get())
+    
+    while not left_sorted_string.empty():
+        output_string.put(left_sorted_string.get())
+    
+    while not right_sorted_stirng.empty():
+        output_string.put(right_sorted_stirng.get())
 
-    return outpu_string
+    return output_string
 
-input_string = input("Ingrese una cadena: ")
-print(strings(input_string))
+# Ejemplo de uso
+input_str = "openai"
+result_queue = strings(input_str)
+
+# Mostrar el contenido de la cola en la consola
+while not result_queue.empty():
+    print(result_queue.queue[0])
+    result_queue.get()
